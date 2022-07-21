eb140ec9c0157026a1c1022920c2aee15eabf301
tausif69 <32447818+tausif69@users.noreply.github.com>
2022-07-21 12:14:52 -0500

    Add files via upload

---
+++ WebTables.js
@@ -0,0 +1,31 @@
+describe('My Test Suite', function() 
+{
+ 
+it('My Test case',function() {
+ 
+//Check boxes
+cy.visit("url")
+ 
+cy.get('tr td:nth-child(2)').each(($e1, index, $list) => {
+ 
+    const text=$e1.text()
+    if(text.includes("Python"))
+    {
+ 
+        cy.get("tr td:nth-child(2)").eq(index).next().then(function(price)
+        {
+         const priceText=   price.text()
+         expect(priceText).to.equal('26')
+        })
+    }
+ 
+})
+ 
+ 
+})
+ 
+ 
+})
+ 
+ 
+ 
